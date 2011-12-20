91f1d2a93cb20822b38b0acb059fea9975acb45b
Huang Wei <imonyse@gmail.com>
2011-12-20 17:25:28 +0800

    sgf parser

--- test/sgf.js
+++ test/sgf.js
@@ -1,9 +1,134 @@
 module('SGFParser');
 
-var sgf_sample_1 = "(;FF[4]GM[1]SZ[19]FG[257:Figure 1]PM[1]PB[Takemiya Masaki]BR[9 dan]PW[Cho Chikun]WR[9 dan]RE[W+Resign]KM[5.5]TM[28800]DT[1996-10-18,19]EV[21st Meijin]RO[2 (final)]SO[Go World #78]US[Arno Hollosi];B[pd];W[dp];B";
+var sgf_sample_1 = "(;FF[4]GM[1]SZ[19]FG[257:Figure 1]PM[1]PB[Takemiya Masaki]BR[9 dan]PW[Cho Chikun]WR[9 dan]RE[W+Resign]KM[5.5]TM[28800]DT[1996-10-18,19]EV[21st Meijin]RO[2 (final)]SO[Go World #78]US[Arno Hollosi];B[pd];W[dp])";
 
-test('next', function(){
+test('nexttoken from custom string', function() {
+  var p = new WeiQi.SGFParser(' ((\n (\t( ss s(\r)'),
+      i = null;
+
+  i = p.nexttoken();
+  equal(i, '(');
+  equal(p.lookahead, '(\n (\t( ss s(\r)');
+  
+  i = p.nexttoken();
+  equal(i, '(');
+  equal(p.lookahead, '\n (\t( ss s(\r)');
+  
+  i = p.nexttoken();
+  equal(i, '(');
+  equal(p.lookahead, '\t( ss s(\r)');
+
+  i = p.nexttoken();
+  equal(i, '(');
+  equal(p.lookahead, ' ss s(\r)');
+  
+  i =p.nexttoken();
+  equal(i, 's');
+  equal(p.lookahead, 's s(\r)');
+
+  i = p.nexttoken();
+  equal(i, 's');
+  equal(p.lookahead, ' s(\r)');
+
+  i = p.nexttoken();
+  equal(i, 's');
+  equal(p.lookahead, '(\r)');
+
+  i = p.nexttoken();
+  equal(i, '(');
+  equal(p.lookahead, '\r)');
+
+  i = p.nexttoken();
+  equal(i, ')');
+  equal(p.lookahead, "");
+
+  i = p.nexttoken();
+  equal(i, null);
+  equal(p.lookahead, null);
+});
+
+test('step_next', function(){
   var p = new WeiQi.SGFParser(sgf_sample_1);
-  p.next();
-  equal(p.lookahead.charAt(0), ';');
+  p.lookahead = "12345";
+
+  p.step_next();
+  equal(p.lookahead, "2345");
+});
+
+test('propvalue', function() {
+  var p = new WeiQi.SGFParser(sgf_sample_1);
+  p.lookahead = "[19]DT[2011-11-11]";
+  equal(p.propvalue(), 19);
+  
+  p.lookahead = "[sss[OMG\\]ok]";
+  equal(p.propvalue(), "sss[OMG]ok");
+});
+
+test('propident', function(){
+  var p = new WeiQi.SGFParser(sgf_sample_1);
+  var v;
+  p.lookahead = "PB[Takemiya Masaki]BR[9 dan]";
+  v = p.propident();
+  equal(v, "PB");
+
+  p.lookahead = "LB[dc:1][fc:2][nc:3][pc:4][dj:a][fj:b][nj:c][pj:d][gs:ABCDEFGH][gr:ABCDEFG][gq:ABCDEF][gp:ABCDE][go:ABCD][gn:ABC][gm:AB][mm:12][mn:123][mo:1234][mp:12345][mq:123456][mr:1234567][ms:12345678]";
+  v = p.propident();
+  equal(v, "LB");
+
+  p.lookahead = "C[ss]";
+  v = p.propident();
+  equal(v, "C");
+});
+
+test('property', function(){
+  var p = new WeiQi.SGFParser(sgf_sample_1),
+      result;
+  p.lookahead = "LB[dc:1]";
+  result = p.property();
+  equal(result.name, "LB");
+  equal(result.value[0], "dc:1");
+
+  p.lookahead = "LB[dc:1][fc:2][nc:3][pc:4][dj:a][fj:b][nj:c][pj:d][gs:ABCDEFGH][gr:ABCDEFG][gq:ABCDEF][gp:ABCDE][go:ABCD][gn:ABC][gm:AB][mm:12][mn:123][mo:1234][mp:12345][mq:123456][mr:1234567][ms:12345678]W[dc]";
+  result = p.property();
+  equal(result.name, "LB");
+  equal(result.value[0], "dc:1");
+  equal(result.value[1], "fc:2");
+  equal(result.value.length, 22);
+  equal(result.value[21], "ms:12345678");
+
+  result = p.property();
+  equal(result.name, "W");
+  equal(result.value, "dc");
+});
+
+test('sequence', function(){
+  var p = new WeiQi.SGFParser(sgf_sample_1),
+      head = {},
+      seq;
+
+  p.lookahead = sgf_sample_1.slice(1);
+  seq = p.sequence(head);
+
+  equal(head._child._property.length, 17);
+  equal(head._child._property[0].name, "FF");
+  equal(head._child._child._property[0].name, "B");
+  equal(head._child._child._child._property[0].name, "W");
+});
+
+test('gametree', function(){
+  var p = new WeiQi.SGFParser(sgf_sample_1),
+      tree;
+
+  tree = p.gametree(null, 's');
+  equal(tree._child._property.length, 17);
+  equal(tree._child._next, null);
+
+  p = new WeiQi.SGFParser(window.ff4_ex);
+  tree = p.gametree(null, 'l');
+
+  p = new WeiQi.SGFParser(window.print1);
+  tree = p.gametree(null, 'l');
+
+  p = new WeiQi.SGFParser(window.print2);
+  tree = p.gametree(null, 'l');
 });
--- weiqi.js
+++ weiqi.js
@@ -29,26 +29,34 @@
   var SGFProperty;
   var SGFNode;
 
-  SGFProperty = function(){
-    
+  SGFProperty = function(name, value){
+    this.name = name || null;
+    if (typeof value !== 'undefined' && value !== null) {
+      this.value = [value];
+    } else {
+      this.value = [];
+    }
   };
 
   _.extend(SGFProperty.prototype, {
-    get: function(name) {
-      
+    get: function() {
+      return this.name;
     },
 
-    add: function(name, value) {
-      
+    add: function(value) {
+      this.value[this.value.length] = value;
     },
 
-    overwrite: function(name, value) {
-      
+    overwrite: function(value) {
+      this.value = value;
     }
   });
 
   SGFNode = function(property){
-    this.property = property || new SGFProperty();
+    this._property = property || new SGFProperty();
+    this._parent = null;
+    this._child = null;
+    this._next = null;
   };
 
   _.extend(SGFNode.prototype, {
@@ -57,10 +65,6 @@
     },
 
     root: function() {
-      
-    },
-
-    makeProperty: function(name, value) {
       
     },
 
@@ -266,26 +270,200 @@
   WeiQi.SGFParser = function(sgf_text) {
     this.sgf_text = sgf_text;
     this.lookahead = this.sgf_text;
-    this.nexttoken = /(;|\(|\))/;
+    this.non_whitespace = /\S/;
     this.STRICT_SGF = 's';
     this.LAX_SGF = 'l';
   };
   
   _.extend(WeiQi.SGFParser.prototype, {
-    next: function() {
-      var i = this.lookahead.search(this.nexttoken);
-      if (i === -1) {
-	this.lookahead = null;
-	return;
-      } else {
-	this.lookahead = this.lookahead.slice(i+1);
-	return this.lookahead;
+    step_next: function(i) {
+      if (typeof i === 'undefined') {
+	i = 0;
       }
+      this.lookahead = this.lookahead.slice(i+1);
+    },
+    
+    // get the next non-whitespace character
+    nexttoken: function() {
+      var token = null,
+          i = 0;
+
+      if (this.lookahead !== null) {
+	// update this.lookahead
+	i = this.lookahead.search(this.non_whitespace);
+	if (i === -1) {
+	  this.lookahead = null;
+	  token = null;
+	} else {
+	  token = this.lookahead.slice(i).charAt(0);
+	  this.step_next(i);
+	}
+      }
+
+      return token;
+    },
+
+    match: function(expected) {
+      if (this.lookahead.charAt(0) !== expected) {
+	throw("expected: " + expected);
+      } else {
+	this.nexttoken();
+      }
+    },
+
+    propvalue: function() {
+      var v = null;
+
+      this.match('[');
+      // regex match couldn't be done here because we have to ignore the
+      // backslash escaping character
+      while (this.lookahead !== null && this.lookahead.charAt(0) !== ']') {
+	if (this.lookahead.charAt(0) === '\\') {
+	  // FF4 definition of backslash
+	  this.step_next();
+	  if (this.lookahead === '\r') {
+	    this.step_next();
+	    if (this.lookahead === '\n') {
+	      this.step_next();
+	    }
+	  }
+	} else if (this.lookahead === '\n') {
+	  this.step_next();
+	  if (this.lookahead === '\r') {
+	    this.step_next();
+	  }
+	}
+	
+	if (v === null) {
+	  v = this.lookahead.charAt(0);
+	} else {
+	  v += this.lookahead.charAt(0);
+	}
+	this.step_next();
+      }
+      
+      this.match(']');
+
+      return v;
+    },
+
+    propident: function() {
+      var name = null,
+          result = null,
+          ident_reg = /^([A-Z]+)(\[)/;
+
+      if (this.lookahead === null) {
+	throw("propident: Expected an upper case letter");
+      }
+
+      result = this.lookahead.match(ident_reg);
+      if (result !== null) {
+	name = result[1];
+      } else {
+	throw("propident: Expected an upper case letter as prop name");
+      }
+
+      this.lookahead = this.lookahead.slice(this.lookahead.indexOf(result[2]));
+      return name;
+    },
+
+    makeProperty: function(name, value) {
+      return new SGFProperty(name, value);
+      
+    },
+
+    // return a property of a node
+    property: function() {
+      var name,
+          value,
+          prop;
+
+      name = this.propident();
+      value = this.propvalue();
+      prop = this.makeProperty(name, value);
+
+      while (this.lookahead.charAt(0) === '[') {
+	value = this.propvalue();
+	prop.add(value);
+      }
+
+      return prop;
+    },
+
+    node: function() {
+      var last = [];
+
+      this.match(';');
+      while (this.lookahead !== null 
+	     && this.lookahead.search(/^[A-Z]+\[/) !== -1) {
+	last[last.length] = this.property();
+      }
+
+      return last;
+    },
+
+    // return the last node of a sequence
+    sequence: function(n) {
+      var seq,
+          new_node,
+          prop;
+      
+      while (this.lookahead.charAt(0) === ';') {
+	prop = this.node();
+	if (prop === null) {
+	  break;
+	}
+
+	new_node = new SGFNode(prop);
+	new_node._parent = n;
+	n._child = new_node;
+	n = new_node;
+      }
+      return n;
+    },
+
+    gametree: function(parent, mode) {
+      if (mode === this.STRICT_SGF) {
+	this.match('(');
+      } else {
+	for (;;) {
+	  if (this.lookahead === null) {
+	    throw("SGFParser.gametree(): Empty String?");
+	  }
+	  if (this.lookahead.charAt(0) === '(') {
+	    while (this.lookahead.charAt(0) === '(') {
+	      this.nexttoken();
+	    }
+	    if (this.lookahead.charAt(0) === ';') {
+	      break;
+	    }
+	  }
+	  this.nexttoken();
+	}
+      }
+
+      // head is parsed
+      var head = {_parent:null, _child:null},
+          last,
+          next;
+
+      last = this.sequence(head);
+      last._next = [];
+      while (this.lookahead.charAt(0) === '(') {
+	next = this.gametree(last, mode);
+	last._next[last._next.length] = next;
+      }
+
+      if (mode === this.STRICT_SGF) {
+	this.match(')');
+      }
+
+      return head;
     },
 
     fuseki: function(parent, mode, moves_per_game, i) {
       if (mode === this.STRICT_SGF) {
-	match(/\(/);
+	this.match('(');
       } else {
 	for (;;) {
 	  if (this.lookahead === null) {
@@ -293,11 +471,20 @@
 	  }
 	  if (this.lookahead.charAt(0) === '(') {
 	    while (this.lookahead.charAt(0) === '(') {
-	      this.next();
+	      this.nexttoken();
+	    }
+	    if (this.lookahead.charAt(0) === ';') {
+	      break;
 	    }
 	  }
+	  this.nexttoken();
 	}
       }
+      
+      var head = {},
+          last;
+      head._parent = parent;
+      
     }
   });
 
